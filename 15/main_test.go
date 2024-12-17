c3189ed06db8173afdb1f45a54cf703ffe8dc73e
samshadwell <git@samshadwell.com>
2024-12-17 13:15:05 -0500

    Day 15 part 2

--- 15/main_test.go
+++ 15/main_test.go
@@ -7,32 +7,33 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestDoMoves(t *testing.T) {
-	board := [][]rune{
-		[]rune("##########"),
-		[]rune("#..O..O.O#"),
-		[]rune("#......O.#"),
-		[]rune("#.OO..O.O#"),
-		[]rune("#..O...O.#"),
-		[]rune("#O#..O...#"),
-		[]rune("#O..O..O.#"),
-		[]rune("#.OO.O.OO#"),
-		[]rune("#....O...#"),
-		[]rune("##########"),
-	}
-	moves := []rune(strings.ReplaceAll(`<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
-vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
-><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
-<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
-^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
-^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
->^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
-<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
-^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
-v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^`, "\n", ""))
-	robot := Location{4, 4}
+var part1Board = [][]rune{
+	[]rune("##########"),
+	[]rune("#..O..O.O#"),
+	[]rune("#......O.#"),
+	[]rune("#.OO..O.O#"),
+	[]rune("#..O@..O.#"),
+	[]rune("#O#..O...#"),
+	[]rune("#O..O..O.#"),
+	[]rune("#.OO.O.OO#"),
+	[]rune("#....O...#"),
+	[]rune("##########"),
+}
 
-	want := `##########
+var part2Board = [][]rune{
+	[]rune("####################"),
+	[]rune("##....[]....[]..[]##"),
+	[]rune("##............[]..##"),
+	[]rune("##..[][]....[]..[]##"),
+	[]rune("##....[]@.....[]..##"),
+	[]rune("##[]##....[]......##"),
+	[]rune("##[]....[]....[]..##"),
+	[]rune("##..[][]..[]..[][]##"),
+	[]rune("##........[]......##"),
+	[]rune("####################"),
+}
+
+var part1Rendered = `##########
 #.O.O.OOO#
 #........#
 #OO......#
@@ -44,18 +45,55 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^`, "\n", "
 ##########
 `
 
-	final, err := DoMoves(nil, board, moves, robot)
-	if err != nil {
-		t.Fatalf("expected DoMoves to result in no error, got: %v", err)
-	}
-	got := Render(board, final)
-	if diff := cmp.Diff(want, got); diff != "" {
-		t.Fatalf("final board does not match expected. Diff (-want +got):\n%s", diff)
-	}
+var part2Rendered = `####################
+##[].......[].[][]##
+##[]...........[].##
+##[]........[][][]##
+##[]......[]....[]##
+##..##......[]....##
+##..[]............##
+##..@......[].[][]##
+##......[][]..[]..##
+####################
+`
 
-	wantPart1 := 10092
-	gotPart1 := Part1(board)
-	if gotPart1 != wantPart1 {
-		t.Fatalf("score for part 1 does not match expected. Want: %d, got: %d", wantPart1, gotPart1)
+var testMoves = []rune(strings.ReplaceAll(`<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^`, "\n", ""))
+
+var tests = []struct {
+	name         string
+	board        [][]rune
+	wantRendered string
+	wantScore    int
+}{
+	{"Part 1", part1Board, part1Rendered, 10092},
+	{"Part 2", part2Board, part2Rendered, 9021},
+}
+
+func TestDoAllMoves(t *testing.T) {
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			final, err := DoAllMoves(nil, tc.board, testMoves)
+			if err != nil {
+				t.Fatalf("expected DoMoves to result in no error, got: %v", err)
+			}
+			got := Render(tc.board, final)
+			if diff := cmp.Diff(tc.wantRendered, got); diff != "" {
+				t.Fatalf("final rendered board does not match expected. Diff (-want +got):\n%s", diff)
+			}
+
+			gotScore := Score(tc.board)
+			if gotScore != tc.wantScore {
+				t.Fatalf("score does not match expected. Want: %d, got: %d", tc.wantScore, gotScore)
+			}
+		})
 	}
 }
