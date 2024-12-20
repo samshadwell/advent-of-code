fb6f7679332c7a7ff5a3ed20ed86347bf12505c4
samshadwell <git@samshadwell.com>
2024-12-20 13:43:21 -0500

    Make day 20 way better

--- 20/main.go
+++ 20/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"advent-of-code/util/grids"
+	"advent-of-code/util/mymath"
 	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
-	"runtime/pprof"
 )
 
 type input struct {
@@ -16,24 +16,6 @@ type input struct {
 }
 
 func main() {
-	// CPU Profile
-	cpuFile, err := os.Create("cpu.prof")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer cpuFile.Close()
-	if err := pprof.StartCPUProfile(cpuFile); err != nil {
-		log.Fatal(err)
-	}
-	defer pprof.StopCPUProfile()
-
-	// Memory Profile
-	memFile, err := os.Create("mem.prof")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer memFile.Close()
-
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatalf("error while opening input file: %v", err)
@@ -53,11 +35,6 @@ func main() {
 
 	fmt.Printf("Part 1: %d\n", part1)
 	fmt.Printf("Part 2: %d\n", part2)
-
-	// Write memory profile at the end
-	if err := pprof.WriteHeapProfile(memFile); err != nil {
-		log.Fatal(err)
-	}
 }
 
 func parseInput(r io.Reader) (input, error) {
@@ -109,87 +86,33 @@ func countGoodCheats(track [][]rune, ordering map[grids.Location]int, cheatSteps
 	numRows := len(track)
 	numCols := len(track[0])
 
-	memo := make(map[locN]*treeNode, cheatSteps*numRows*numCols)
 	for start := range ordering {
-		accessible := locationsNStepsAway(track, start, cheatSteps, memo)
-		accessible.visitValues(func(end grids.Location) {
-			timeSaved := ordering[end] - ordering[start] - distance(start, end)
-			if timeSaved >= threshold {
-				count++
+		startOrd := ordering[start]
+		for numUp := -cheatSteps; numUp <= cheatSteps; numUp++ {
+			stepsRemaining := cheatSteps - mymath.IntAbs(numUp)
+			for numRight := -stepsRemaining; numRight <= stepsRemaining; numRight++ {
+				amountUp := grids.Up().ScalarMult(numUp)
+				amountSide := grids.Left().ScalarMult(numRight)
+
+				end := start.Plus(amountUp).Plus(amountSide)
+				if grids.IsOutOfBounds(end, numRows, numCols) || track[end.Row][end.Col] == '#' {
+					continue
+				}
+
+				timeSaved := ordering[end] - startOrd - distance(start, end)
+				if timeSaved >= threshold {
+					count++
+				}
 			}
-		})
+		}
 	}
 
 	return count
 }
 
-// Used as key in our memoization of results
-type locN struct {
-	loc grids.Location
-	n   int
-}
-
-type treeNode struct {
-	hasValue bool
-	value    grids.Location
-	children []*treeNode
-}
-
-func (tn *treeNode) visitValues(f func(grids.Location)) {
-	visited := make(map[*treeNode]bool)
-	tn.visitValuesHelper(visited, f)
-}
-
-func (tn *treeNode) visitValuesHelper(visited map[*treeNode]bool, f func(grids.Location)) {
-	if visited[tn] {
-		return
-	}
-	visited[tn] = true
-	if tn.hasValue {
-		f(tn.value)
-	}
-	for _, c := range tn.children {
-		c.visitValuesHelper(visited, f)
-	}
-}
-
-func locationsNStepsAway(track [][]rune, start grids.Location, n int, memo map[locN]*treeNode) *treeNode {
-	key := locN{start, n}
-	if res, ok := memo[key]; ok {
-		return res
-	}
-
-	res := &treeNode{}
-	memo[key] = res
-
-	if n == 0 {
-		if track[start.Row][start.Col] != '#' {
-			res.hasValue = true
-			res.value = start
-		}
-		return res
-	}
-
-	children := make([]*treeNode, 0, 5)
-	children = append(children, locationsNStepsAway(track, start, n-1, memo))
-	grids.EachAdjacent(start, len(track), len(track[0]), func(loc grids.Location) {
-		children = append(children, locationsNStepsAway(track, loc, n-1, memo))
-	})
-
-	res.children = children
-	return res
-}
-
 func distance(start, end grids.Location) int {
 	deltaR := start.Row - end.Row
-	if deltaR < 0 {
-		deltaR = -deltaR
-	}
-
 	deltaC := start.Col - end.Col
-	if deltaC < 0 {
-		deltaC = -deltaC
-	}
 
-	return deltaR + deltaC
+	return mymath.IntAbs(deltaR) + mymath.IntAbs(deltaC)
 }
