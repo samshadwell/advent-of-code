bcb4718cb7a9475737a810936692c0100c04ff8f
Sam Shadwell <git@samshadwell.com>
2025-12-01 15:57:10 -0500

    Modify template to use tests (#10)

--- 2025/src/bin/NN.rs
+++ 2025/src/bin/NN.rs
@@ -7,43 +7,57 @@ use std::io::{BufRead, BufReader};
 const DAY: &str = "NN"; // TODO: Fill the day
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
-const TEST: &str = "\
-<TEST-INPUT>
-"; // TODO: Add the test input
+// TODO: Change result type and implement
+fn part1<R: BufRead>(_reader: R) -> Result<i32> {
+    Ok(0)
+}
+
+// TODO: Change result type and implement
+fn part2<R: BufRead>(_reader: R) -> Result<i32> {
+    Ok(0)
+}
 
 fn main() -> Result<()> {
     start_day(DAY);
 
     //region Part 1
     println!("=== Part 1 ===");
-
-    fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        // TODO: Solve Part 1 of the puzzle
-        let answer = reader.lines().map_while(Result::ok).count();
-        Ok(answer)
-    }
-
-    // TODO: Set the expected answer for the test input
-    assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
-
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = part1(input_file)?;
     println!("Result = {}", result);
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = part2(input_file)?;
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = part2(input_file)?;
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST: &str = "\
+<TEST-INPUT>
+"; // TODO: Add the test input
+
+    #[test]
+    fn part_1() {
+        // TODO: Modify expected
+        let expected = 0;
+        let result = part1(BufReader::new(TEST.as_bytes()));
+        assert_eq!(result.unwrap(), expected)
+    }
+
+    #[test]
+    fn part_2() {
+        // TODO: Modify expected
+        let expected = 0;
+        let result = part2(BufReader::new(TEST.as_bytes()));
+        assert_eq!(result.unwrap(), expected)
+    }
 }
