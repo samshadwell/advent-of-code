0ba24a4d0070ea57b4a61d0204fb50c95e7bb14b
samshadwell <git@samshadwell.com>
2025-12-20 19:32:44 -0500

    Solve 2015 day 03
    Results:
    ```
    Advent of Code 2025 - Day 03
    === Parsing input ===
    Parsing time = 50.82µs
    
    === Part 1 ===
    Result = <redacted>
    Elapsed = 277.77µs
    
    === Part 2 ===
    Result = <redacted>
    Elapsed = 246.26µs
    ```

---
+++ 2015/src/bin/03.rs
@@ -0,0 +1,86 @@
+use adv_code_2015::start_day;
+use anyhow::{Result, anyhow};
+use const_format::concatcp;
+use std::collections::HashSet;
+use std::time::Instant;
+
+const DAY: &str = "03";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+fn do_move(curr: (i32, i32), dir: char) -> Result<(i32, i32)> {
+    match dir {
+        '<' => Ok((curr.0 - 1, curr.1)),
+        '>' => Ok((curr.0 + 1, curr.1)),
+        '^' => Ok((curr.0, curr.1 - 1)),
+        'v' => Ok((curr.0, curr.1 + 1)),
+        _ => Err(anyhow!("unknown direction: {}", dir)),
+    }
+}
+
+fn part1(input: &str) -> Result<usize> {
+    let mut seen = HashSet::new();
+    let mut curr = (0, 0);
+    seen.insert(curr);
+    for c in input.chars() {
+        curr = do_move(curr, c)?;
+        seen.insert(curr);
+    }
+    Ok(seen.len())
+}
+
+fn part2(input: &str) -> Result<usize> {
+    let mut seen = HashSet::new();
+    let mut positions = [(0, 0), (0, 0)];
+    seen.insert((0, 0));
+    for (idx, c) in input.chars().enumerate() {
+        let which = idx % 2;
+        let curr = positions[which];
+        let new = do_move(curr, c)?;
+        seen.insert(new);
+        positions[which] = new;
+    }
+
+    Ok(seen.len())
+}
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    println!("=== Parsing input ===");
+    let parse_time = Instant::now();
+    let input = std::fs::read_to_string(INPUT_FILE)?;
+    println!("Parsing time = {:.2?}\n", parse_time.elapsed());
+
+    println!("=== Part 1 ===");
+    let p1_time = Instant::now();
+    let result = part1(&input)?;
+    println!("Result = {}", result);
+    println!("Elapsed = {:.2?}", p1_time.elapsed());
+
+    println!("\n=== Part 2 ===");
+    let p2_time = Instant::now();
+    let result = part2(&input)?;
+    println!("Result = {}", result);
+    println!("Elapsed = {:.2?}", p2_time.elapsed());
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1() {
+        assert_eq!(2, part1(">").unwrap());
+        assert_eq!(4, part1("^>v<").unwrap());
+        assert_eq!(2, part1("^v^v^v^v^v").unwrap());
+    }
+
+    #[test]
+    fn part_2() {
+        assert_eq!(3, part2("^v").unwrap());
+        assert_eq!(3, part2("^>v<").unwrap());
+        assert_eq!(11, part2("^v^v^v^v^v").unwrap());
+    }
+}
